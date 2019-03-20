10f96b6860cf3aab0246abc47e38a9fe719549b3
Abdullateef <yomlateef@yahoo.com>
2019-03-20 04:52:03 +0100

    Started working on RoboFriends

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Card = () => {
+    return (
+        <div>
+            <img alt="robot" src="https://robohash.org/test?200x200" />
+            <div>
+                <h2>John Doe</h2>
+                <p>John.Doe@gmail.com</p>
+            </div>
+        </div>
+    ) 
+}
+
+export default Card
