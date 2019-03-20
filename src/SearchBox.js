75cc815006c4a6db83048077c747c6d46cb2207a
Abdullateef <yomlateef@yahoo.com>
2019-03-20 11:53:56 +0100

    Created a state, also feeling sleepy

---
+++ src/SearchBox.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const searchBox = () => {
+    return (
+        <input 
+            type='search'
+            className="pa3 ba b--green bg-lightest-blue"
+            placeholder="Search"
+        />
+    )
+}
+
+export default searchBox;
