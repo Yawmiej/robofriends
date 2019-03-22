92421c112f315864f5fdad35ca14d4400d522c1c
Abdullateef <yomlateef@yahoo.com>
2019-03-22 11:11:21 +0100

    Organized components using Architecture

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Card = ({name, email, id}) => {
     return (
-        <div className="bg-light-green dib br3 pa3 ma2 bw3 grow tc shadow-5">
+        <div className="bg-light-green dib br3 pa3 ma2 bw3 grow tc shadow-5 crd">
             <img alt="robot" src={`https://robohash.org/${id}?size=200x200`} />
             <div>
                 <h2>{name}</h2>
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,26 +1,31 @@
 import React, {Component} from "react";
-import CardList from "./CardList"
-import SearchBox from "./SearchBox";
+import CardList from "../components/CardList"
+import SearchBox from "../components/SearchBox";
+import {robots} from "../robot";
+import Scroll from "../components/Scroll";
 
 
 class App extends Component {
     constructor (){
         super();
         this.state = {
-            robot: [],
+            robot: robots,
             searchInput: ""
         }
     }
 
     render () {
-        const filteredRobot = this.state.robot.filter(robot => {
-            return robot.name.toLowerCase().includes(this.state.searchInput.toLowerCase());
+        const {robot, searchInput} = this.state
+        const filteredRobot = robot.filter(robot => {
+            return robot.name.toLowerCase().includes(searchInput.toLowerCase());
         })
         return (
             <div className="tc">
                 <h1>Robofriends</h1>
                 <SearchBox searchChange = {this.onSearchChange}/>
-                <CardList robots={filteredRobot}/>
+                <Scroll>
+                    <CardList robots={filteredRobot}/>
+                </Scroll>
             </div>
         )
     }
